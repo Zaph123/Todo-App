c0e4a824c5cdc46cae8d005e58841eca22a8a3d0
Einstein Bassey <einsteinbassey@gmail.com>
2025-05-22 02:21:51 +0100

    ready for production and fixed .env issues

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -10,10 +10,10 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "next-gen-todo-app-94e5f",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:236372083203:web:6daf08a3eef61d3460c436",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 
